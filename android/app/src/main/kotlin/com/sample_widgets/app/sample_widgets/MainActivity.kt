1c08a084a37fc09d8af3b3bc6b674a4ba55d8d47
Jainish-twoiq <jainish.shah@twoiq.com>
2021-10-01 16:14:13 +0530

    home_page

---
+++ android/app/src/main/kotlin/com/sample_widgets/app/sample_widgets/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.sample_widgets.app.sample_widgets
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/home_screen.dart
@@ -0,0 +1,77 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+
+class homePage extends StatefulWidget {
+  const homePage({Key? key}) : super(key: key);
+
+  @override
+  _homePageState createState() => _homePageState();
+}
+
+class _homePageState extends State<homePage> {
+  void _showBottom() {
+    showModalBottomSheet<void>(
+        context: context,
+        builder: (BuildContext context) {
+          return Container(
+            padding: const EdgeInsets.all(15.0),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                const Text(
+                  'Click Here For Next Page',
+                  style: TextStyle(color: Colors.red),
+                ),
+                ElevatedButton(
+                  onPressed: () => Navigator.pop(context),
+                  child: const Text('Next'),
+                )
+              ],
+            ),
+          );
+        });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Sample App'),
+        centerTitle: true,
+        backgroundColor: Colors.blue,
+      ),
+      body: SizedBox(
+        width: double.infinity,
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          //mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            ElevatedButton(
+                onPressed: () {}, child: const Text('Elevated Button')),
+            const SizedBox(height: 30.0),
+            FlatButton(onPressed: () {}, child: const Text('Flat Button')),
+            const SizedBox(height: 30.0),
+          ],
+        ),
+      ),
+      endDrawer: Drawer(
+        child: Column(
+          // mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: const <Widget>[
+            Text(
+              'Hello World',
+              style: TextStyle(fontSize: 18.0, fontFamily: 'Poppins-Black'),
+            ),
+            Divider(
+              height: 15.0,
+              color: Colors.red,
+            ),
+          ],
+        ),
+      ),
+      bottomSheet: ElevatedButton(
+          onPressed: _showBottom, child: const Text('Click Here')),
+    );
+  }
+}
